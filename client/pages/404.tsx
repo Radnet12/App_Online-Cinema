8cb3737396cb0d6bf754085bfd17443d444d5f2c
Sergey Vasylenko <sergeyvasylenko4@gmail.com>
2022-04-03 10:34:33 +0300

    added 404 and 500 errors

---
+++ client/pages/404.tsx
@@ -0,0 +1,11 @@
+import { Heading, Meta } from "@/ui";
+
+const Error404 = () => {
+  return (
+    <Meta title="Page not found">
+      <Heading title="404 - Page Not Found" />
+    </Meta>
+  );
+};
+
+export default Error404;
---
+++ client/pages/500.tsx
@@ -0,0 +1,11 @@
+import { Heading, Meta } from "@/ui";
+
+const Error500 = () => {
+  return (
+    <Meta title="Server-side error">
+      <Heading title="500 - Server-side error occurred" />
+    </Meta>
+  );
+};
+
+export default Error500;
